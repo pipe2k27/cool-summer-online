cb2d12bab58d2f53735bbb3da0733d4ae09d7e7b
Tobias Mancini <tobias_mancini@hotmail.com>
2022-10-05 18:05:33 -0300

    segundo push

--- src/Components/Construccion/index.js
+++ src/Components/Construccion/index.js
@@ -19,45 +19,15 @@ const Construccion = () => {
       </div>
 
       <div className="mainText">
-        <p>
-          Nos destacamos por construir piletas con la más alta teconología.
-          Se realizará la excavación en forma manual o con maquina. Se
-          confeccionara un entramado de hierro aletados de 6 mm formando una
-          malla soldada, cubriendo piso, paredes, vereda perimetral y solario de
-          igual sección e idéntica armadura, el piso y los chanfles se
-          hormigonaran en un espesor uniforme de 15 cm.
-        </p>
-
         <h2>
           HORMIGÓN PROYECTADO
         </h2>
-        <p className="paragraph">
+        <p>
           Este sistema es mas rapido y brinda una solidez superior al tradicional mixto de ladrillo y hormigón armado
           Al ser proyectado en la totalidad de la superficie en un solo día (en el espacio de un  par de horas) brinda una estructura monolitica de hormigon a toda la pileta.
           El hormigon h30 que se utiliza para la construcción, es preparado en la planta de fabricacion por computadoras y procesado para ser transpotado y utilizado en la superficie de metros prevista.
           La estructura dela pileta, esta armada con doble maya de hierros, de 6mm y de 8mm, que recubre toda la superficie, se prepara in situ, según los requerimientos del terreno (no se colocan mayas armadas estandar), en COOL SUMMER le brindamos especial dedicacion y cuidado a este y cada uno de los detalles que hacen de su pileta un producto de exelencia y calidad superior al estandar.
           Las otras piletas son parecidas, las nuestras son unicas!!!
-        </p>
-        <h2>
-          HORMIGÓN ARMADO
-        </h2>
-
-        <p >
-          Se confeccionara una columna central a lo largo de la pileta y tres
-          columnas a lo ancho (en forma de costillas) para darle mayor firmeza a
-          la estructura de hierro de la superficie total de la pileta. A un
-          metro de profundidad desde el nivel superior hasta la altura de 90 cm.
-          Se realizara una viga perimetral donde se asentaran ladrillos en forma
-          de panderete cumpliendo la función de encofrado perdido, entre dicho
-          encofrado y el suelo natural se hará una colada de hormigón.
-        </p>
-        <p>
-          En las paredes y el piso se realizara un azotado de hidrófugo y se
-          revocara listo para pintar. La vereda perimetral será de 50 cm. De
-          ancho en todo el perímetro de la pileta asentada en cemento y cal
-          sobre el hormigón realizado, el ancho de la vereda que supere los 50
-          cm. Se considerara como solario. El solario tendrá la misma armadura y
-          sección que el resto de la estructura.
         </p>
       </div>
     </div>
