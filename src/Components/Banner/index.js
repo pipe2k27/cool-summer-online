86e888e1842bc9525204bdbf4ea6a76f794cf5d7
felipe perarnau <feliperarnau@gmail.com>
2022-10-28 15:50:23 -0300

    fix home images import

--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,21 +1,31 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
-import { RiToolsFill } from 'react-icons/ri';
-import { TbShovel } from 'react-icons/tb';
+import { RiToolsFill } from "react-icons/ri";
+import { TbShovel } from "react-icons/tb";
 import ControlledCarousel from "../Carousel";
 
 const Banner = () => {
-
-  const bannerImages = ["/images/piletaBC1.JPG","/images/banner.JPG", "/images/banner2.JPG", "/images/banner3.JPG"]
+  const bannerImages = [
+    "/images/piletaBC1.jpg",
+    "/images/banner.jpg",
+    "/images/banner2.jpg",
+    "/images/banner3.jpg",
+  ];
 
   return (
     <div>
       <div className="banner1">
         <div className="mainBG">
           <span className="mainOverlay"></span>
-          <img className="main-banner" src={process.env.PUBLIC_URL + "/images/water-20.gif"}></img>
-          <img className="main-logo" src={process.env.PUBLIC_URL + "/images/logo-no-letters.png"}></img>
+          <img
+            className="main-banner"
+            src={process.env.PUBLIC_URL + "/images/water-20.gif"}
+          ></img>
+          <img
+            className="main-logo"
+            src={process.env.PUBLIC_URL + "/images/logo-no-letters.png"}
+          ></img>
           <h1>CONSTRUÍ TU PROPIO ESTILO</h1>
           <h2>Disfrutalo a tu manera</h2>
           <div className="main-btn-container">
@@ -35,14 +45,17 @@ const Banner = () => {
         </div>
       </div>
       <div className="mainText">
-        <h2>
-          ¿QUÉ HACEMOS?
-        </h2>
+        <h2>¿QUÉ HACEMOS?</h2>
         <p>
-          Somos una empresa, con una vasta experiencia y trayectoria en el mercado de la construcción, refacción y modernización de piletas.
-          Contamos con más de 20 años de experiencia, un gran equipo de trabajo, para la realización de cada trabajo, maquinarias de primer nivel y sobre todo, pasión por lo que hacemos.
-          Brindamos asesoramiento e ideas revolucionarias y modernas. Contamos con accesorios de ultima generación, tanto para la construcción como para la refacción y modernización de la pileta, y estamos muy atentos a las nuevas tendencias y novedades del mercado.
-
+          Somos una empresa, con una vasta experiencia y trayectoria en el
+          mercado de la construcción, refacción y modernización de piletas.
+          Contamos con más de 20 años de experiencia, un gran equipo de trabajo,
+          para la realización de cada trabajo, maquinarias de primer nivel y
+          sobre todo, pasión por lo que hacemos. Brindamos asesoramiento e ideas
+          revolucionarias y modernas. Contamos con accesorios de ultima
+          generación, tanto para la construcción como para la refacción y
+          modernización de la pileta, y estamos muy atentos a las nuevas
+          tendencias y novedades del mercado.
         </p>
       </div>
       <ControlledCarousel array={bannerImages} vof={false} />
