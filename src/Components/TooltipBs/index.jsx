4abaca84a354fd2e1e5a87a4c8550831d99748df
felipe perarnau <feliperarnau@gmail.com>
2022-11-02 22:51:10 -0300

    added magic erase

--- src/Components/TooltipBs/index.jsx
+++ src/Components/TooltipBs/index.jsx
@@ -1,27 +1,36 @@
-import Button from 'react-bootstrap/Button';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-import { RiWhatsappFill } from 'react-icons/ri';
-import './styles.css';
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import { RiWhatsappFill } from "react-icons/ri";
+import "./styles.css";
 
 function TooltipBS() {
-    const renderTooltip = (props) => (
-        <Tooltip id="button-tooltip" {...props}>
-            Contactanos
-        </Tooltip>
-    );
+  const renderTooltip = (props) => (
+    <Tooltip id="button-tooltip" {...props}>
+      Contactanos
+    </Tooltip>
+  );
 
-    return (
-        <OverlayTrigger
-            placement="left"
-            delay={{ show: 250, hide: 300 }}
-            overlay={renderTooltip}
+  return (
+    <OverlayTrigger
+      placement="left"
+      delay={{ show: 250, hide: 300 }}
+      overlay={renderTooltip}
+    >
+      <Button variant="success">
+        <a
+          href="https://api.whatsapp.com/send?phone=5491144949207&amp;text=Buen%20día,%20te%20hago%20una%20consulta:"
+          target="_blank"
         >
-            <Button variant="success">
-                <a href="https://api.whatsapp.com/send?phone=5491144949207&amp;text=Buen%20día,%20te%20hago%20una%20consulta:" target="_blank"><RiWhatsappFill className="waIcon" /></a>
-            </Button>
-        </OverlayTrigger>
-    );
+          <img
+            src={process.env.PUBLIC_URL + "/images/wa-icon.png"}
+            alt="w"
+            className="waIcon"
+          ></img>
+        </a>
+      </Button>
+    </OverlayTrigger>
+  );
 }
 
 export default TooltipBS;
